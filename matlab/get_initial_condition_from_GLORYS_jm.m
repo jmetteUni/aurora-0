72aea1871d7696dfd5a03c4ff8a0769ed926a906
JMette <jmette@uni-bremen.de>
2024-12-02 17:07:55 +0100

    renamed files with wrong gridsize

--- matlab/get_initial_condition_from_GLORYS_jm.m
+++ matlab/get_initial_condition_from_GLORYS_jm.m
@@ -1,4 +1,4 @@
-% This program is used to create initial condition files from NEMO data
+scp% This program is used to create initial condition files from NEMO data
 %created by G.Xu
 %modified by J. Mette, 20/04/2024
 
